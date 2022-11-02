8083215355dd2aa64c1d3094520d11a57b52c8b0
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-11-02 17:51:06 -0400

    Create Number8.java

---
+++ Number8.java
@@ -0,0 +1,13 @@
+int getNumberOfZerosLoop(int n) {                  
+  int k, count =0;
+  while(n > 0)         
+  {             
+    k = n % 10;             
+    if(k==0)             
+   count=count+1;             
+    n = n / 10;         
+    
+  }        
+  return count;     
+  
+}
